14d4c9c3968a0947ef1a8c0ecc83abd5a3459f78
Shahriyor <shahriyoradhamov@icloud.com>
2024-02-29 20:12:03 +0500

    feat: overview is ready

--- src/components/overview/overview.tsx
+++ src/components/overview/overview.tsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import { Box, Flex, Grid, Heading, Icon, Text } from '@chakra-ui/react';
+import { useTranslation } from 'react-i18next';
+import { BsCheck } from 'react-icons/bs';
+import { GoDotFill } from "react-icons/go";
 
 const Overview = () => {
-  return (
-    <div>Overview</div>
-  );
+	const { t } = useTranslation();
+
+	const whatYouLearn = 'AJAX, JavaScript, Fetch, OOP, API, JSON, Promise';
+	const requirement = 'Basic HTML, CSS, JavaSciprt, SASS, Advanced API';
+
+	return (
+		<>
+			<Heading mt={10}>{t('overview', { ns: 'courses' })}</Heading>
+			<Text mt={3}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam inventore ducimus aliquam incidunt culpa sint, consequuntur, quam earum facere provident quasi, suscipit corrupti voluptate nobis?
+			</Text>
+			<Heading mt={10}>{t('what_you_will_learn', { ns: 'courses' })}</Heading>
+			<Grid
+				mt={5}
+				gridTemplateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' }}
+			>
+				{whatYouLearn.split(', ').map((text, idx) => (
+					<Flex key={idx} gap={3} align={'center'} my={1}>
+						<Icon as={BsCheck} w={6} h={6} borderRadius={'100%'} p={1} />
+						<Text>{text}</Text>
+					</Flex>
+				))}
+			</Grid>
+			<Heading mt={10}>{t('required', { ns: 'courses' })}</Heading>
+			<Box mt={3}>
+				{requirement.split(', ').map((text, idx) => (
+					<Flex key={idx} gap={2} align={'center'}>
+						<Icon as={GoDotFill} w={5} h={5} />
+						<Text>{text}</Text>
+					</Flex>
+				))}
+			</Box>
+		</>
+	);
 };
 
 export default Overview;
